4bb4655b5d897d1be26b91e490d70c2da1ba5018
Laura <di.wu2018@outlook.com>
2020-08-30 15:29:35 -0700

    updates for 08/30/2020 distance between two nodes in BST

---
+++ src/TreeNode.java
@@ -0,0 +1,18 @@
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val){
+        this.val = val;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
