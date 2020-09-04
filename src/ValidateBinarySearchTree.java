a86ea4662df9c10d045f2ddd027f60afb3a1c4a9
Laura <di.wu2018@outlook.com>
2020-09-04 16:11:07 -0700

    updates for 09/04/2020 validate binary search tree recrusion way

---
+++ src/ValidateBinarySearchTree.java
@@ -0,0 +1,22 @@
+public class ValidateBinarySearchTree {
+
+    //time:O(n) space:O(n)  recrusion way
+    public boolean isValidBST(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    //node should be in the range of (min, max)
+    private boolean isValid(TreeNode node, long min, long max){
+        if(node == null){
+            return true;
+        }
+        if(node.val <= min || node.val >= max){
+            return false;
+        }
+
+        return isValid(node.left, min, node.val) && isValid(node.right, node.val, max);
+    }
+}
