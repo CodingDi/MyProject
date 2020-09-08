fc3189b2166c646b4d2b5646c988b356f5871bb1
Laura <di.wu2018@outlook.com>
2020-09-08 14:35:12 -0700

    update for 08/09  DFS for binary tree

---
+++ src/MissingBinarySearch.java
@@ -0,0 +1,28 @@
+public class MissingBinarySearch {
+
+    //sorted array, from 1-n find the one that is missing from the array
+    public int findMissing(int[] arr){
+        if(arr == null|| arr.length == 0) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = arr.length - 1;
+        while(left < right - 1){
+            int mid = left + (right - left)/2;
+            if(arr[mid] == mid + 1){
+                left = mid;
+            }else{
+                right = mid;
+            }
+        }
+        //post-processing
+        if(arr[left] != left + 1){
+            return left + 1;
+        }
+        if(arr[right] != right + 1){
+            return right + 1;
+        }
+        return right + 2;
+    }
+}
