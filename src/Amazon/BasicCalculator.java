2a36f41a214bdbf1af09102ac361125781ae48b3
Laura <di.wu2018@outlook.com>
2020-09-16 18:04:40 -0500

    update for  9/16 basic calculator update for the consistent method!!  with recursion 1.遇到左括号就 call recursion. need to update the i's position by  j + 1 2.if its digits, record all digits in the while loop, remember to increase i++ to avoid dead loop 3. for operands + - or the last character, we use it as the previous operands

--- src/Amazon/BasicCalculator.java
+++ src/Amazon/BasicCalculator.java
@@ -4,40 +4,71 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class BasicCalculator {
+    class Solution {
         public int calculate(String s) {
-            if(s == null ||s.length() == 0){
+            //corner case
+            if(s == null || s.length() == 0){
                 return 0;
             }
 
             Deque<Integer> stack = new ArrayDeque<>();
+            char sign = '+';//initialize the sign as +
             int tmp = 0;
-            int sign = 1;
 
             for(int i = 0; i < s.length(); i++){
                 char cur = s.charAt(i);
+
                 if(Character.isDigit(cur)){
                     int num = cur - '0';
-                    while(i + 1< s.length() && Character.isDigit(s.charAt(i + 1))){
-                        num = num*10 + s.charAt(i+1) - '0';
+
+                    while(i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))){
+                        num = num*10 + s.charAt(i + 1) - '0';
                         i++;
                     }
-                    tmp += sign*num;
-                }else if(cur == '+'){
-                    sign = 1;
-                }else if(cur == '-'){
-                    sign = -1;
+                    tmp = num;
                 }else if(cur == '('){
-                    stack.offerFirst(tmp);
-                    stack.offerFirst(sign);
+                    int j = i + 1;
+                    int diff = 1;//the difference between left and right brackets
 
+                    for(;j < s.length(); j++){
+                        if(s.charAt(j) == '('){
+                            diff++;
+                        }else if(s.charAt(j) == ')'){
+                            diff--;
+                        }
+
+                        //when equal,we can cut can call recursion
+                        if(diff == 0){
+                            break;
+                        }
+                    }
+
+                    tmp = calculate(s.substring(i + 1, j));//call recursion for expression in the brackets
+                    i = j;//update the i position
+                }
+
+                //deal with operands: push to the stack
+                if(cur == '+' || cur == '-' || i == s.length() - 1){
+                    if(sign == '+'){
+                        stack.offerFirst(tmp);
+                    }else if(sign == '-'){
+                        stack.offerFirst(-tmp);
+                    }
+
+                    //reset the tmp variable and the sign
+                    sign = cur;
                     tmp = 0;
-                    sign = 1;
-                }else if(cur == ')'){
-                    tmp =tmp*stack.pollFirst() + stack.pollFirst();
                 }
             }
 
-            return tmp;
+            int res = 0;
+
+            while(!stack.isEmpty()){
+                res += stack.pollFirst();
+            }
+
+            return res;
         }
+    }
 
 }
