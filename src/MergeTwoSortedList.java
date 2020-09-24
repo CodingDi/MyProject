d2c14d1da62c7267bd67ce41e9337d3b04a1d40e
Laura <di.wu2018@outlook.com>
2020-09-24 03:05:38 -0500

    update for  9/23 merge two sorted list

---
+++ src/MergeTwoSortedList.java
@@ -0,0 +1,13 @@
+
+public class MergeTwoSortedList {
+  int val;
+  MergeTwoSortedList next;
+   MergeTwoSortedList() {}
+   MergeTwoSortedList(int val) {
+       this.val = val;
+   }
+   MergeTwoSortedList(int val, MergeTwoSortedList next) {
+       this.val = val; this.next = next;
+   }
+}
+
