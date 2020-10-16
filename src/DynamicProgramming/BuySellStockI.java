709ca67e99dd5178de12accdbc12d5ec1249591f
Laura <di.wu2018@outlook.com>
2020-10-15 19:50:50 -0500

    update for  10/15 sell and buy stock I

---
+++ src/DynamicProgramming/BuySellStockI.java
@@ -0,0 +1,23 @@
+package DynamicProgramming;
+
+public class BuySellStockI {
+
+        public int maxProfit(int[] prices) {
+            if (prices == null || prices.length <= 1) {
+                return 0;
+            }
+
+            int minPrice = Integer.MAX_VALUE;
+            int maxProfit = 0;
+
+            for (int i = 0; i < prices.length; i++) {
+                if (prices[i] < minPrice) {
+                    minPrice = prices[i];
+                }
+                maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            }
+
+            return maxProfit;
+        }
+
+}
